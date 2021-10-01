d8f8eedc782149a90c8950bcea11a09723536f79
DavidQuesillo <davidgl50@hotmail.com>
2021-10-01 09:51:39 -0300

    Initial commit

---
+++ Assets/scripts/Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rb;
+
+    [SerializeField]
+    private float hSpeed = 1f;
+    [SerializeField]
+    private float vSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveHorizontal();
+    }
+
+    private void MoveHorizontal()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * hSpeed, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.back * hSpeed, ForceMode.Force);
+        }
+    }
+}
